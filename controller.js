b5d3bd456dded6abad513212ae4b5e510e31a4df
AbdulRehman <likeam99@gmail.com>
2023-01-06 18:53:50 +0500

    css settings

---
+++ controller.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=20e329e5f78f5fce809c3e59cdab6a5b';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=20e329e5f78f5fce809c3e59cdab6a5b';
